ce4b5b06b275850ab6d50efd83bd613e3e7e4e89
fernando12128 <fernando.augusto12128@gmail.com>
2024-10-14 00:44:54 -0300

    Subindo exercicio

---
+++ ex 34 Algoritmo/script.js
@@ -0,0 +1,14 @@
+let x = parseInt(prompt("Digite um numero: "))
+let y = parseInt(prompt("Digite um numero: "))
+
+let z = (x * y) + 5
+
+if(z<= 0 ){
+    alert("A")
+}else if(z <= 100){
+    alert("B")
+}else{
+    alert("C")
+}
+
+
